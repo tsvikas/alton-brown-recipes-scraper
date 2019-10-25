5520c5c9a67b9fdb2ad36e51e7509da267dbeca5
Tsvika S <tsvikas@gmail.com>
2019-10-25 16:14:51 +0300

    feat: download images

--- AB_recipes/settings.py
+++ AB_recipes/settings.py
@@ -64,9 +64,11 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    'AB_recipes.pipelines.AbRecipesPipeline': 300,
-# }
+ITEM_PIPELINES = {
+    "scrapy.pipelines.images.ImagesPipeline": 1,
+    # 'AB_recipes.pipelines.AbRecipesPipeline': 300,
+}
+IMAGES_STORE = "scraped_data/images"
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
